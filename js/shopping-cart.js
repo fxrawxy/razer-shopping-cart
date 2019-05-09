8f8e51845a4c7289bcd67039cc1bb359b9aa27aa
fxrawxy <fxrawxy@yandex.com>
2019-05-09 22:29:38 +0500

    local storage support

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -5,6 +5,7 @@ const basket = document.querySelector('.basket'),
 	itemAmount = document.querySelector('.i-amount'),
 	shoppingCart = document.querySelector('.shopping-cart'),
 	shoppingCartCont = document.querySelector('.shopping-cart tbody'),
+	clearCartBtn = document.querySelector('.remove-order'),
 	items = document.querySelector('.catalog-body');
 
 
@@ -12,11 +13,20 @@ const basket = document.querySelector('.basket'),
 // events
 
 function addEvents() {
+	// open shopping cart
 	basketLink.addEventListener('click', openShoppingCart);
 
+	// add item to cart
 	items.addEventListener('click', buyItem);
 
+	// remove item from cart
 	shoppingCartCont.addEventListener('click', removeFromCart);
+
+	// clear cart
+	clearCartBtn.addEventListener('click', clearCart);
+
+	// print items from local storage
+	document.addEventListener('DOMContentLoaded', printFromStorage);
 }
 
 addEvents();
@@ -26,7 +36,6 @@ addEvents();
 // functions
 
 // open shopping cart
-
 function openShoppingCart() {
 	if (itemAmount.classList.contains('active')) {
 		basketLink.classList.toggle('active');
@@ -39,10 +48,9 @@ function openShoppingCart() {
 	}
 }
 
+// add to cart 1
 function buyItem(e) {
 	e.preventDefault();
-
-	// use delegation to find item was added
 
 	if (e.target.classList.contains('add-to-cart')) {
 		const item = e.target.parentElement.parentElement.parentElement;
@@ -52,22 +60,18 @@ function buyItem(e) {
 }
 
 // get item info
-
 function getItemInfo(item) {
-
-	// create object with item data
-
 	const itemInfo = {
 		image: item.querySelector('img').src,
 		title: item.querySelector('.card-name').textContent,
-		price: item.querySelector('.card-price').textContent
+		price: item.querySelector('.card-price').textContent,
+		id: item.querySelector('.add-to-cart').getAttribute('data-id')
 	}
 
 	addToCart(itemInfo);
 }
 
-// add item to basket
-
+// add to cart 2
 function addToCart(item) {
 	const row = document.createElement('tr');
 
@@ -77,43 +81,125 @@ function addToCart(item) {
 		</td>
 		<td>${item.title}</td>
 		<td>${item.price}</td>
-		<td><ion-icon name="close" class="remove"></ion-icon></td>
+		<td><ion-icon name="close" class="remove" data-id="${item.id}"></ion-icon></td>
 	`;
 
 	shoppingCartCont.appendChild(row);
 
-	// update current amount of items in basket
-
 	itemAmount.innerHTML = shoppingCartCont.childElementCount;
-
-	// enable shopping cart and add 'in basket' effect
 
 	if (shoppingCartCont.childElementCount > 0) {
 		itemAmount.classList.add('active');
 	}
+
+	saveIntoStorage(item);
 }
 
-// remove item from basket
+// save into local storage
+function saveIntoStorage(item) {
+	let items = getFromStorage();
 
-function removeFromCart(e) {
-	if (e.target.classList.contains('remove')) {
-		e.target.parentElement.parentElement.remove();
+	items.push(item);
+
+	localStorage.setItem('items', JSON.stringify(items));
+}
+
+// get from local storage
+function getFromStorage() {
+	let items;
+
+	if (localStorage.getItem('items') === null) {
+		items = [];
+	} else {
+		items = JSON.parse(localStorage.getItem('items'));
 	}
 
-	// update current amount of items in basket
+	return items;
+}
+
+// print from local storage
+function printFromStorage() {
+	let itemsLS = getFromStorage();
+
+	itemsLS.forEach(function(item) {
+		const row = document.createElement('tr');
+
+		row.innerHTML = `
+			<td>
+				<img src="${item.image}">
+			</td>
+			<td>${item.title}</td>
+			<td>${item.price}</td>
+			<td><ion-icon name="close" class="remove" data-id="${item.id}"></ion-icon></td>
+		`;
+
+		shoppingCartCont.appendChild(row);
+
+		itemAmount.innerHTML = shoppingCartCont.childElementCount;
+
+		if (shoppingCartCont.childElementCount > 0) {
+			itemAmount.classList.add('active');
+		}
+	});
+}
+
+// remove from cart
+function removeFromCart(e) {
+	let item, itemId;
+
+	if (e.target.classList.contains('remove')) {
+		e.target.parentElement.parentElement.remove();
+
+		item = e.target.parentElement.parentElement;
+		itemId = item.querySelector('ion-icon').getAttribute('data-id');
+	}
+
+	removeFromStorage(itemId);
 
 	itemAmount.innerHTML = shoppingCartCont.childElementCount;
 
-	// if all items was removed
-
 	if (shoppingCartCont.childElementCount === 0) {
-		// hide shopping cart
 		shoppingCart.style.display = '';
 
-		// remove 'basket opened' effect
 		basketLink.classList.remove('active');
 
-		// remove 'in basket' effect
 		itemAmount.classList.remove('active')
 	}
+}
+
+// remove from local storage
+function removeFromStorage(id) {
+	let itemsLS = getFromStorage();
+
+	itemsLS.forEach(function(itemLS, index) {
+		if (itemLS.id === id) {
+			itemsLS.splice(index, 1);
+		}
+	});
+
+	localStorage.setItem('items', JSON.stringify(itemsLS));
+}
+
+// clear cart
+function clearCart() {
+	while (shoppingCartCont.firstChild) {
+		shoppingCartCont.removeChild(shoppingCartCont.firstChild);
+	}
+
+	itemAmount.innerHTML = shoppingCartCont.childElementCount;
+
+	if (shoppingCartCont.childElementCount === 0) {
+		shoppingCart.style.display = '';
+
+		basketLink.classList.remove('active');
+
+		itemAmount.classList.remove('active')
+	}
+
+	clearStorage();
+}
+
+// clear local storage
+function clearStorage() {
+	localStorage.clear();
 }
